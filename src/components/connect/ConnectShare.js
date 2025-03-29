eea0c72ed84a971de1f23372ebd1547585220c73
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2025-03-29 10:52:05 +0000

    Aider prompt: In ConnectShare we need to update settings slice but also filter slice for muni- correct

--- src/components/connect/ConnectShare.js
+++ src/components/connect/ConnectShare.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux'
 import { useLocation } from 'react-router-dom'
 
 import { updateSettings } from '../../redux/settingsSlice'
+import { updateSelection } from '../../redux/updateSelection'
 import { useAppHistory } from '../../utils/useAppHistory'
 
 /**
@@ -21,48 +22,53 @@ const ConnectShare = () => {
     const overlay = searchParams.get('overlay')
     const muni = searchParams.get('muni')
 
-    const updates = {}
+    const settingsUpdates = {}
+    const filterUpdates = {}
 
     if (mapType) {
-      updates.mapType = mapType
+      settingsUpdates.mapType = mapType
     }
 
     if (showLabels === 'true') {
-      updates.showLabels = true
+      settingsUpdates.showLabels = true
     }
 
     if (overlay) {
       // Convert from URL format (bicycle/transit) to layer format (BicycleLayer/TransitLayer)
       if (overlay === 'bicycle') {
-        updates.overlay = 'BicycleLayer'
+        settingsUpdates.overlay = 'BicycleLayer'
       } else if (overlay === 'transit') {
-        updates.overlay = 'TransitLayer'
+        settingsUpdates.overlay = 'TransitLayer'
       } else {
-        updates.overlay = overlay
+        settingsUpdates.overlay = overlay
       }
     }
 
     if (muni === 'false') {
-      updates.muni = false
+      filterUpdates.muni = false
     }
 
-    if (Object.keys(updates).length > 0) {
-      // Update Redux state with parameters from URL
-      dispatch(updateSettings(updates))
+    // Update Redux states with parameters from URL
+    if (Object.keys(settingsUpdates).length > 0) {
+      dispatch(updateSettings(settingsUpdates))
+    }
 
-      // Remove the parameters from URL after processing
-      if (mapType) {
-        history.removeParam('mapType')
-      }
-      if (showLabels) {
-        history.removeParam('showLabels')
-      }
-      if (overlay) {
-        history.removeParam('overlay')
-      }
-      if (muni) {
-        history.removeParam('muni')
-      }
+    if (Object.keys(filterUpdates).length > 0) {
+      dispatch(updateSelection(filterUpdates))
+    }
+
+    // Remove the parameters from URL after processing
+    if (mapType) {
+      history.removeParam('mapType')
+    }
+    if (showLabels) {
+      history.removeParam('showLabels')
+    }
+    if (overlay) {
+      history.removeParam('overlay')
+    }
+    if (muni) {
+      history.removeParam('muni')
     }
   }, [location.search, dispatch, history])
 
