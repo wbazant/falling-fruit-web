5a151bbb3b81f9b829e99994a8d0e42fe57a14f7
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2025-04-21 10:12:55 +0100

    Checking in before running aider: Unify the formatting of labels: we want 'days ago' up to 14 days, and the dates for older sections

--- src/components/activity/ChangesPeriod.jsx
+++ src/components/activity/ChangesPeriod.jsx
@@ -9,6 +9,7 @@ import { MIN_LOCATION_ZOOM } from '../../constants/map'
 import { setLastBrowsedSection } from '../../redux/activitySlice'
 import { viewToString } from '../../utils/appUrl'
 import { useIsDesktop } from '../../utils/useBreakpoint'
+import { formatISOString } from '../entry/textFormatters'
 
 const formatChangeType = (type, t) => {
   switch (type) {
@@ -157,12 +158,6 @@ const formatPeriodName = (daysAgo, t) => {
   }
 }
 
-const formatCalendarDate = (date, language) => new Date(date).toLocaleDateString(language, {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  })
-
 const ChangesPeriod = ({ period, userId, useCalendarDates = false }) => {
   const dispatch = useDispatch()
   const isDesktop = useIsDesktop()
@@ -182,7 +177,7 @@ const ChangesPeriod = ({ period, userId, useCalendarDates = false }) => {
     <div id={period.daysAgo}>
       <h3>
         {useCalendarDates
-          ? formatCalendarDate(period.date, i18n.language)
+          ? formatISOString(period.date, i18n.language)
           : formatPeriodName(period.daysAgo, t)}
       </h3>
       <ListChanges>
